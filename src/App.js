39d9f6a03bd6d74d51b0cf2d560cea24e77f38f7
Ollie Relph <ollie@relph.me>
2016-09-06 11:27:25 +0100

    Switch to using Field rather than fields

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React, { Component, PropTypes, } from 'react';
-import { reduxForm, } from 'redux-form';
+import { reduxForm, Field } from 'redux-form';
 
 import Dropzone from 'react-dropzone';
 
 const FILE_FIELD_NAME = 'files';
 
+
+const renderDropzoneInput = (field) => {
+  const files = field.input.value;
+  return (
+    <div>
+      <Dropzone
+        name={field.name}
+        onDrop={ ( filesToUpload, e ) => field.input.onChange(filesToUpload)}
+      >
+        <div>Try dropping some files here, or click to select files to upload.</div>
+      </Dropzone>
+      {field.meta.touched &&
+        field.meta.error &&
+        <span className="error">{field.meta.error}</span>}
+      {files && Array.isArray(files) && (
+        <ul>
+          { files.map((file, i) => <li key={i}>{file.name}</li>) }
+        </ul>
+      )}
+    </div>
+  );
+}
 
 class App extends Component {
 
@@ -38,19 +60,11 @@ class App extends Component {
     return (
       <form onSubmit={ handleSubmit(this.onSubmit.bind(this)) }>
         <div>
-          <label>Files</label>
-          <div>
-            <Dropzone
-              onDrop={ ( filesToUpload, e ) => files.onChange(filesToUpload)}
-            >
-              <div>Try dropping some files here, or click to select files to upload.</div>
-            </Dropzone>
-            { files && Array.isArray(files.value) && (
-              <ul>
-                { files.value.map((file, i) => <li key={i}>{file.name}</li>) }
-              </ul>
-            ) }
-          </div>
+          <label htmlFor={FILE_FIELD_NAME}>Files</label>
+          <Field
+            name={FILE_FIELD_NAME}
+            component={renderDropzoneInput}
+          />
         </div>
         <div>
           <button type="submit">
